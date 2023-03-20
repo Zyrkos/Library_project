3d55a61a58861cc924dadcc4b76b01e5728ff6fc
Pedro Castro <106441980+Zyrkos@users.noreply.github.com>
2023-03-20 00:52:36 +0000

    Add files via upload
    added the animation to the right side bar

--- script.js
+++ script.js
@@ -3,16 +3,11 @@ const sideBarRight = document.getElementById("side-bar-right");
 const mainContainerGrid = document.getElementById("main-container");
 
 infoBtn.addEventListener("click", function () {
-    if (sideBarRight.style.display === "none") {
-      sideBarRight.style.display = "block";
-      mainContainerGrid.style.gridTemplateColumns = "1fr 4fr 1fr";
-    } else {
-      sideBarRight.style.display = "none";
-      mainContainerGrid.style.gridTemplateColumns = "1fr 4fr";
-    }
-  });
+  sideBarRight.classList.toggle("deployed");
 
-  /* function deploySidebar() {
-    var sidebar = document.querySelector('.side-bar-right');
-    sidebar.classList.toggle('deployed');
-  } */
+  if (sideBarRight.classList.contains("deployed")) {
+    mainContainerGrid.style.gridTemplateColumns = "1fr 4fr 1fr";
+  } else {
+    mainContainerGrid.style.gridTemplateColumns = "1fr 4fr";
+  }
+});
